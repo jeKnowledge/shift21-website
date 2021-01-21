7fb503c5c34f1f9e0b7c70e003c1f2dbc1a5c513
jbtsousa <jbtsousa@gmail.com>
2021-01-21 21:12:21 +0000

    minor adjustments

--- showMenu.js
+++ showMenu.js
@@ -1,22 +1,27 @@
     function showMenu(){
 var menu= document.getElementById("menu");
 var nav = document.getElementById("nav");
+var nav_=document.getElementsByTagName("nav");
 var tigre = document.getElementById("tigre_mobile");
 var img = document.getElementById("menu_icon");
 
 
        if (window.getComputedStyle(menu).display === "none" ){
-        menu.style.display="inline";
+        menu.style.display="block";
         menu.style.backgroundColor="#000000";
         tigre.style.display="block";
         img.src= "images/close.svg"
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${window.scrollY}px`;
+
 
 
 }
       else {
       menu.style.display="none";
       img.src= "images/menu.png"
-
+      document.body.style.position = '';
+      document.body.style.top = '';
 
 
    }}
