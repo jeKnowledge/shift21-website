b55480200a02885e76e1ba4fadb67cd8e169a879
jbtsousa <jbtsousa@gmail.com>
2021-01-21 13:54:52 +0000

    minor adjustments, anim gallery done (?)

--- showMenu.js
+++ showMenu.js
@@ -7,16 +7,16 @@ var img = document.getElementById("menu_icon");
 
        if (window.getComputedStyle(menu).display === "none" ){
         menu.style.display="inline";
-        nav.style.height="100%";
         menu.style.backgroundColor="#000000";
         tigre.style.display="block";
-        nav.style.zIndex="3";
         img.src= "images/close.svg"
+
 
 }
       else {
       menu.style.display="none";
       img.src= "images/menu.png"
+
 
 
    }}
