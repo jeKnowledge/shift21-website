06bee159fbf19498fc57da519db4738ba0b23914
jbtsousa <jbtsousa@gmail.com>
2021-04-05 22:05:19 +0100

    teste

--- showMenu.js
+++ showMenu.js
@@ -11,7 +11,6 @@ var img = document.getElementById("menu_icon");
         img.src= "images/close.svg"
         document.body.style.position = 'fixed';
         document.body.style.top = `-${window.scrollY}px`;
-
 }
       else {
       menu.style.display="none";
