a0c681ad011241d258eb8bc2c5998c2b9bf3cc90
jbtsousa <jbtsousa@gmail.com>
2021-04-05 18:19:54 +0100

    added prizes, guide

--- showMenu.js
+++ showMenu.js
@@ -1,7 +1,5 @@
     function showMenu(){
-var menu= document.getElementById("menu");
-var nav = document.getElementById("nav");
-var nav_=document.getElementsByTagName("nav");
+var menu= document.getElementById("menu_nav");
 var tigre = document.getElementById("tigre_mobile");
 var img = document.getElementById("menu_icon");
 
@@ -13,8 +11,6 @@ var img = document.getElementById("menu_icon");
         img.src= "images/close.svg"
         document.body.style.position = 'fixed';
         document.body.style.top = `-${window.scrollY}px`;
-
-
 
 }
       else {
